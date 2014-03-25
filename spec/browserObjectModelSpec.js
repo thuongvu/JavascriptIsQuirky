20e0b7956bd824c6b7f4310767f95de4667ebb8a
thuongvu <thuongvuho@gmail.com>
2014-03-25 16:29:16 -0700

    add - outline for browser object model specs

---
+++ spec/browserObjectModelSpec.js
@@ -0,0 +1,16 @@
+describe("", function() {});
+describe("Browser Object Model", function() {
+	describe("Global Scope", function() {
+
+	});
+	describe("Window position", function() {});
+	describe("Window Size", function() {});
+	describe("Opening windows", function() {});
+	describe("Intervals and timeouts", function() {});
+	describe("Location Object", function() {
+		describe("Query String arguments", function() {});
+		describe("Manipulating Location", function() {});
+		describe("Navigator Object", function() {});
+	}); // location object end
+	describe("History object", function() {});
+}); // brwoser object model end
