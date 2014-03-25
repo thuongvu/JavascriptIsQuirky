eb58f0531e845c006caf4f4b2ed1b1d85ce6fa42
thuongvu <thuongvuho@gmail.com>
2014-03-24 19:34:34 -0700

    add - a little more on variable declaration, skipped a few

--- spec/varScopeMemorySpec.js
+++ spec/varScopeMemorySpec.js
@@ -54,8 +54,36 @@ describe("Variables, Scope, Memory", function() {
 	}); // Passing arguments end
 	describe("In JS, think of function arguments as local variables.", function() {});
 	describe("Execution context and scope", function() {
+		describe("Each execution context has a variable object unpon which all of its defined variables and functions exist.  it is not accessible to a developer", function() {});
+		describe("When a function runs, it creates an execution context, which has variables and functions on it for it to execute, and this context is pushed onto the top of of a context stack", function() {});
+		describe("When it has finished executing, the stack is popped, returning control to the previous executing context", function() {});
+		describe("When a function is executed, a scope chain of variable objects are created, to provide ordered access to all  variables and functions that an execution contet has access to", function() {});
+		describe("The front of the scope chain is the variable object of the context whose code is executing", function() {});
+		describe("If the context is a function, then the activation object is used as the variable object", function() {});
+		describe("An activation object starts with a single defined variable called arguments, and then the next variable object in the chain is from the containing context, and so on, until the global object is reached", function() {});
+//TO DO EXAMPLES PAGE 91-22
 		describe("Variable Declaration", function() {
+			describe("When a variable is declared using var, it is automatically added to te most immediate context available", function() {});
+			it("If a variable is initialized without first being declared, it gets added to the global context automatically", function() {
+				function add(num1, num2) {
+					var sum = num1 + num2;
+					return sum;
+				};
+				var result = add(10,20);
+				expect(result).toEqual(30);
+				expect(function() {sum}).toThrow(new ReferenceError("sum is not defined"));
+				// this causes an error because sum is not accessible outside the function
 
+				// in this one, sumGlobal does not have a var keyword, so it is initialized to a value without ever being declared
+				// therefore it is created in the global context
+				function addGlobal(num1, num2) {
+					sumGlobal = num1 + num2;
+					return sumGlobal;
+				}
+				var resultGlobal = addGlobal(10,20);
+				expect(sumGlobal).toEqual(30);
+				expect(resultGlobal).toEqual(30);
+			});
 		}); // Variable Declaration end
 		describe("Identifier Lookup", function() {
 
