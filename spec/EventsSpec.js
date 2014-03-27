51238dff7525a353f619f8a957d4237dcaf67e66
thuongvu <thuongvuho@gmail.com>
2014-03-27 15:40:10 -0700

    add - html + dom level 0 event handlers

--- spec/EventsSpec.js
+++ spec/EventsSpec.js
@@ -1,10 +1,71 @@
 describe("", function() {});
 it("", function() {});
 
+function htmlEventOnclick(event) {
+	console.log("htmlEventOnclick clicked, and the event is");
+	console.log(event);
+};
 describe("Events", function() {
 	describe("Event Handlers", function() {
-		describe("HTML Event Handlers", function() {});
-		describe("DOM level 0 Event Handlers", function() {});
+		describe("HTML Event Handlers", function() {
+			describe("with event handlers defined in html attributes, they are are wrapped in a function, and have access to the event object", function() {});
+			describe("Also, within this function, document and the element itself can be accessed as if they were local variables", function() {});
+			describe("Cons", function() {
+				describe("The user might try to interact with the element before the event handler is ready, you can deal with this by try/catching it", function() {});
+				describe("The augmentation of scope chain in the event handler function varies across browsers", function() {});
+				describe("It couples the html to javascript, so you'd have to change event handler in html + in js", function() {});
+			});
+		});
+		describe("DOM level 0 Event Handlers", function() {
+			it("Assigning an event handler", function() {
+				var clicked = 0;
+				expect(clicked).toEqual(0);
+
+				// get a reference to the object
+				var domLevelZeroEventBtn = document.getElementById("domLevelZeroEventBtn");
+
+				// add it as a method
+				domLevelZeroEventBtn.onclick = function() {
+					clicked = 1;
+				};
+				domLevelZeroEventBtn.onclick();
+				expect(clicked).toEqual(1);
+
+				// the onclick event hndler isn't assigned until the code is run, so if the user interacts with the page bfore its loaded, the nthe button won't do anything
+			});
+			it("The event handler is considereda method of the element, therefore it is run within the scope of the element, with this = element", function() {
+				var domLevelZeroEventBtn = document.getElementById("domLevelZeroEventBtn");
+				var clicked = null;
+				domLevelZeroEventBtn.onclick = function() {
+					clicked = this.id;
+				};
+				domLevelZeroEventBtn.onclick();
+
+				expect(clicked).toBe('domLevelZeroEventBtn');
+			});
+			it("You can remove an event handler assigned via DOM Level 0 by seeting the value of the event handler property to 0", function() {
+				var domLevelZeroEventBtn = document.getElementById("domLevelZeroEventBtn");
+				var clicked = 0;
+				domLevelZeroEventBtn.onclick = function() {
+					clicked++;
+				};
+				domLevelZeroEventBtn.onclick();
+
+				expect(clicked).toEqual(1);
+
+				// remove the event listener
+				domLevelZeroEventBtn.onclick = null;
+
+				// let's try to invoke the onclick again - it will throw an error
+				expect(function() {
+
+					domLevelZeroEventBtn.onclick();
+
+				}).toThrow(new TypeError("Property 'onclick' of object #<HTMLButtonElement> is not a function"))
+
+			});
+			
+		});
 		describe("DOM level 2 Event Handlers", function() {});
 		describe("Internet Explorer Event Handlers", function() {});
 		describe("Cross browser event handlers", function() {});
@@ -15,14 +76,8 @@ describe("Events", function() {
 			describe("UI", function() {});
 			describe("Focus", function() {});
 			describe("Wheel", function() {});
-			describe("Text", function() {});
-			describe("Keyboard", function() {});
-			describe("Composition", function() {});
-			describe("Mutation events", function() {});
-			describe("Mutation name events", function() {});
 		});
 		describe("HTML5 Events", function() {
-			describe("Context menu", function() {});
 			describe("BeforeUnload", function() {});
 			describe("DOMContentLoaded Event", function() {});
 			describe("readystatechange Event", function() {});
@@ -45,3 +100,7 @@ describe("Events", function() {
 
 describe("", function() {});
 it("", function() {});
+
+
+
+
