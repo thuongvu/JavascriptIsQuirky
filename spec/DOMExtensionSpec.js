84601bc0c5a1b338c1b78b011afa51152237a86d
thuongvu <thuongvuho@gmail.com>
2014-03-26 18:46:14 -0700

    add - selectors api

--- spec/DOMExtensionSpec.js
+++ spec/DOMExtensionSpec.js
@@ -1,10 +1,33 @@
 describe("", function() {});
 describe("DOM Extensions", function() {
 	describe("Selectors API", function() {
-		describe("The querySelector() Method", function() {});
-		describe("The querySelectorAll() Method", function() {});
-		describe("The matchesSelector() method", function() {});
-		describe("Element Traversal", function() {});
+		it("The querySelector() Method accepts a css query and returns the first descendant element that matches the pattern, else null", function() {
+			var body = document.querySelector("body");
+
+			var div1 = document.querySelector("#div1");
+			expect(div1.id).toBe("div1");
+
+			var li = document.querySelector("ul li");
+			expect( li.childNodes[0].data ).toBe("1"); // i expect the text in the first ul > li to be "1"
+		});
+		it("The querySelectorAll() Method accepts one argument, a css query, and returns all matching nodes as a NodeList object, else null", function() {
+			var li = document.querySelectorAll("#div1 ul li");
+			expect(li.length).toEqual(3);
+		});
+		describe("Element Traversal", function() {
+			it("5 properties of DOM elements: childElementcount, firstElementChild, lastElementChild, previousElementSibling, nextElementSibling", function() {
+				var ul = document.querySelector("#div1 ul");
+				expect(ul.childElementCount).toEqual(3); 									// 3 li's
+				expect(ul.firstElementChild.childNodes[0].data).toEqual("1");		// first li
+				expect(ul.lastElementChild.childNodes[0].data).toEqual("3");		// third li
+				var firstLi = ul.firstElementChild;											// 1st li
+				var secondLi = firstLi.nextElementSibling;
+				var thirdLi = ul.lastElementChild;
+				var anotherSecondLi = thirdLi.previousElementSibling;
+				expect(secondLi.childNodes[0].data).toEqual("2");
+				expect(anotherSecondLi.childNodes[0].data).toEqual("2");
+			});
+		});
 	}); // selectors API end
 	describe("HTML5", function() {
 		describe("Class", function() {
