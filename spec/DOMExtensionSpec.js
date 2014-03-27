255bd290e66f1e70d73575deabcb6e166ad30407
thuongvu <thuongvuho@gmail.com>
2014-03-26 17:47:30 -0700

    add - dom extensions

---
+++ spec/DOMExtensionSpec.js
@@ -0,0 +1,34 @@
+describe("", function() {});
+describe("DOM Extensions", function() {
+	describe("Selectors API", function() {
+		describe("The querySelector() Method", function() {});
+		describe("The querySelectorAll() Method", function() {});
+		describe("The matchesSelector() method", function() {});
+		describe("Element Traversal", function() {});
+	}); // selectors API end
+	describe("HTML5", function() {
+		describe("Class", function() {
+			describe("getElementsByClassName() method", function() {});
+			describe("classList property", function() {});
+		}); // Class end
+		describe("Changes to HTML5 Document", function() {
+			describe("Focus", function() {});
+			describe("readyState property", function() {});
+			describe("Markup insertion", function() {
+				describe("innerHTML", function() {});
+				describe("outerHTML", function() {});
+				describe("insertAdjacentHTML()", function() {});
+			});
+		}); // Changes to HTML5 Document end
+	}); // HTML5 end
+	describe("Propietary extensions", function() {
+		describe("children Property", function() {});
+		describe("contains() method", function() {});
+	}); // Propietary extensions end
+}); //"DOM Extensions" end
+
+
+
+
+describe("", function() {});
+it("", function() {});
